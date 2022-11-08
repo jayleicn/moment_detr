17b4c40b6f0b8bb007ebac64d2aa12e146eb0154
monsi <simon.goldhofer@gmail.com>
2022-11-08 16:09:52 +0000

    added val set

---
+++ moment_detr/scripts/train_mad.sh
@@ -0,0 +1,37 @@
+cd ../../
+dset_name=hl
+ctx_mode=video_tef
+v_feat_types=clip
+t_feat_type=clip 
+results_root=results
+exp_id=exp
+
+######## data paths
+train_path=/nfs/data3/goldhofer/mad_dataset/annotations/MAD_train_transformed.json
+eval_path=/nfs/data3/goldhofer/mad_dataset/annotations/MAD_val_transformed.json
+eval_split_name=val
+
+######## setup video+text features
+v_feat_dirs=(/nfs/data3/goldhofer/mad_dataset/)
+v_feat_dim=512
+t_feat_dir=/nfs/data3/goldhofer/mad_dataset/
+t_feat_dim=512
+#### training
+bsz=32
+
+
+PYTHONPATH=$PYTHONPATH:. python moment_detr/train.py \
+--dset_name ${dset_name} \
+--ctx_mode ${ctx_mode} \
+--train_path ${train_path} \
+--eval_path ${eval_path} \
+--eval_split_name ${eval_split_name} \
+--v_feat_dirs ${v_feat_dirs[@]} \
+--v_feat_dim ${v_feat_dim} \
+--t_feat_dir ${t_feat_dir} \
+--t_feat_dim ${t_feat_dim} \
+--bsz ${bsz} \
+--results_root ${results_root} \
+--num_workers 12 \
+--exp_id ${exp_id} \
+${@:1}
--- utils/preprocess_mad.py
+++ utils/preprocess_mad.py
@@ -2,35 +2,57 @@ import json, os
 import numpy as np
 from tqdm import tqdm
 
-SPLIT = 'train'
 root = '/nfs/data3/goldhofer/mad_dataset'
-train_data = json.load(open(f'{root}/annotations/MAD_{SPLIT}.json', 'r'))
-moment_detr_dict = {}
-mad_transformed = []
-cnt = 0
-for k in tqdm(train_data.keys()):
-    lowest_clip = int(np.floor(round(train_data[k]["timestamps"][0])))
-    highest_clip = int(np.ceil(round(train_data[k]["timestamps"][1])))
-    if lowest_clip % 2 != 0:
-        lowest_clip -= 1
-    if highest_clip % 2 != 0:
-        highest_clip += 1
+mad_transformed_train = []
+mad_transformed_val = []
 
-    if highest_clip > train_data[k]["movie_duration"]:
-        highest_clip = int(np.floor(train_data[k]["movie_duration"]))
+for mad_path in [ f'{root}/annotations/MAD_val.json',f'{root}/annotations/MAD_train.json']:
+    train_data = json.load(open(mad_path, 'r'))
+    moment_detr_dict = {}
 
-    if highest_clip - lowest_clip > 0:
-        moment_detr_dict = {"qid": k + "_" + train_data[k]["movie"], "query": train_data[k]["sentence"],
-                            "duration": train_data[k]["movie_duration"],
-                            "vid": train_data[k]["movie"],
-                            "relevant_windows": [lowest_clip, highest_clip],
-                            "relevant_clip_ids": [i for i in range(int(lowest_clip / 2), int(highest_clip / 2))],
-                            "saliency_scores": [[0, 0, 0] for i in range(int(lowest_clip / 2), int(highest_clip / 2))]}
-        mad_transformed.append(moment_detr_dict)
-    else:
-        cnt += 1
+    cnt = 0
+    for k in tqdm(list(train_data.keys())[0:100]):
+        lowest_clip = int(np.floor(round(train_data[k]["timestamps"][0])))
+        highest_clip = int(np.ceil(round(train_data[k]["timestamps"][1])))
+        if lowest_clip % 2 != 0:
+            lowest_clip -= 1
+        if highest_clip % 2 != 0:
+            highest_clip += 1
 
-with open(f'{root}/annotations/MAD_{SPLIT}_transformed.json', "w") as f:
-    f.write("\n".join([json.dumps(e) for e in mad_transformed]))
+        if highest_clip > train_data[k]["movie_duration"]:
+            highest_clip = int(np.floor(train_data[k]["movie_duration"]))
 
-print(f'# Clip duration probably zero: {cnt}')
+        if highest_clip - lowest_clip > 0:
+
+            if "train" in mad_path:
+                moment_detr_dict = {"qid": k + "_" + train_data[k]["movie"],
+                                    "query": train_data[k]["sentence"],
+                                    "duration": train_data[k]["movie_duration"],
+                                    "vid": train_data[k]["movie"],
+                                    "relevant_windows": [lowest_clip, highest_clip],
+                                    "relevant_clip_ids": [i for i in
+                                                          range(int(lowest_clip / 2), int(highest_clip / 2))],
+                                    "saliency_scores": [[0, 0, 0] for i in
+                                                        range(int(lowest_clip / 2), int(highest_clip / 2))]}
+                mad_transformed_train.append(moment_detr_dict)
+            else:
+                moment_detr_dict = {"qid": k + "_" + train_data[k]["movie"].split("_")[0],
+                                    "query": train_data[k]["sentence"],
+                                    "duration": train_data[k]["movie_duration"],
+                                    "vid": train_data[k]["movie"],
+                                    "relevant_windows": [lowest_clip, highest_clip],
+                                    "relevant_clip_ids": [i for i in
+                                                          range(int(lowest_clip / 2), int(highest_clip / 2))],
+                                    "saliency_scores": [[0, 0, 0] for i in
+                                                        range(int(lowest_clip / 2), int(highest_clip / 2))]}
+                mad_transformed_val.append(moment_detr_dict)
+        else:
+            cnt += 1
+    print(f'# Clip duration for {mad_path} probably zero: {cnt}')
+
+with open(f'{root}/annotations/MAD_train_transformed.json', "w") as f:
+    f.write("\n".join([json.dumps(e) for e in mad_transformed_train]))
+with open(f'{root}/annotations/MAD_val_transformed.json', "w") as f:
+    f.write("\n".join([json.dumps(e) for e in mad_transformed_val]))
+
+print(f'length train: {len(mad_transformed_train)}, length val: {len(mad_transformed_val)}')
