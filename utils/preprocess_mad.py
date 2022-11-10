fd0a7ff859c37f96cd88e4fa4d59e8e71127a0cc
monsi <simon.goldhofer@gmail.com>
2022-11-10 11:13:11 +0000

    fast commit

--- utils/preprocess_mad.py
+++ utils/preprocess_mad.py
@@ -4,73 +4,69 @@ from tqdm import tqdm
 import h5py
 import pickle
 
-root = '/nfs/data3/goldhofer/mad_dataset'
-mad_transformed_train = []
-mad_transformed_val = []
 
-for mad_path in [f'{root}/annotations/MAD_val.json', f'{root}/annotations/MAD_train.json']:
-    train_data = json.load(open(mad_path, 'r'))
-    moment_detr_dict = {}
+def run():
+    root = '/nfs/data3/goldhofer/mad_dataset'
+    mad_transformed_train = []
+    mad_transformed_val = []
+    rng = np.random.default_rng(42)
 
-    cnt = 0
-    for k in tqdm(list(train_data.keys())):
-        lowest_clip = int(np.floor(round(train_data[k]["ext_timestamps"][0])))
-        highest_clip = int(np.ceil(round(train_data[k]["ext_timestamps"][1])))
-        if lowest_clip % 2 != 0:
-            lowest_clip -= 1
-        if highest_clip % 2 != 0:
-            highest_clip += 1
+    for mad_path in [f'{root}/annotations/MAD_val.json', f'{root}/annotations/MAD_train.json']:
+        train_data = json.load(open(mad_path, 'r'))
 
-        if highest_clip > train_data[k]["movie_duration"]:
-            highest_clip = int(np.floor(train_data[k]["movie_duration"]))
+        cnt = 0
+        for k in tqdm(list(train_data.keys())):
+            lowest_clip = int(np.floor(round(train_data[k]["ext_timestamps"][0])))
+            highest_clip = int(np.ceil(round(train_data[k]["ext_timestamps"][1])))
+            if lowest_clip % 2 != 0:
+                lowest_clip -= 1
+            if highest_clip % 2 != 0:
+                highest_clip += 1
 
-        if highest_clip - lowest_clip > 0:
+            if highest_clip > train_data[k]["movie_duration"]:
+                highest_clip = int(np.floor(train_data[k]["movie_duration"]))
 
-            moment_detr_dict = {"qid": k + "_" + train_data[k]["movie"],
-                                "query": train_data[k]["sentence"],
-                                "duration": train_data[k]["movie_duration"],
-                                "vid": train_data[k]["movie"],
-                                "relevant_windows": [[lowest_clip, highest_clip]],
-                                "relevant_clip_ids": [i for i in
-                                                      range(int(lowest_clip / 2), int(highest_clip / 2))],
-                                "saliency_scores": [[0, 0, 0] for i in
-                                                    range(int(lowest_clip / 2), int(highest_clip / 2))]}
+            if highest_clip - lowest_clip > 0:
+                video_feat_cache, meta = _slice_window(video_feat_cache, meta, rng)
 
-            if len(moment_detr_dict["saliency_scores"]) != 0:
-                assert len(moment_detr_dict["relevant_windows"][0]) != 0
-                assert len(moment_detr_dict["saliency_scores"]) != 0, "saliency scores are zero"
-                assert moment_detr_dict["relevant_windows"][0][0] < moment_detr_dict["relevant_windows"][0][1]
-                assert 0 <= moment_detr_dict["relevant_windows"][0][0] < moment_detr_dict["relevant_windows"][0][
-                    1], f'relevant window: {moment_detr_dict["relevant_windows"][0]}'
+                moment_detr_dict = {"qid": k + "_" + train_data[k]["movie"],
+                                    "query": train_data[k]["sentence"],
+                                    "duration": train_data[k]["movie_duration"],
+                                    "vid": train_data[k]["movie"],
+                                    "relevant_windows": [[lowest_clip, highest_clip]],
+                                    "relevant_clip_ids": [i for i in
+                                                          range(int(lowest_clip / 2), int(highest_clip / 2))],
+                                    "saliency_scores": [[0, 0, 0] for i in
+                                                        range(int(lowest_clip / 2), int(highest_clip / 2))]}
 
-            if "train" in mad_path:
-                mad_transformed_train.append(moment_detr_dict)
+                check_dict(moment_detr_dict)
+                if len(moment_detr_dict["saliency_scores"]) != 0:
+                    if "train" in mad_path:
+                        mad_transformed_train.append(moment_detr_dict)
+                    else:
+                        mad_transformed_val.append(moment_detr_dict)
 
             else:
-                mad_transformed_val.append(moment_detr_dict)
-
-        else:
-            cnt += 1
-    print(f'# Clip duration for {mad_path} probably zero: {cnt}')
-
-with open(f'{root}/annotations/MAD_train_transformed.json', "w") as f:
-    f.write("\n".join([json.dumps(e) for e in mad_transformed_train]))
-with open(f'{root}/annotations/MAD_val_transformed.json', "w") as f:
-    f.write("\n".join([json.dumps(e) for e in mad_transformed_val]))
-
-print(f'length train: {len(mad_transformed_train)}, length val: {len(mad_transformed_val)}')
+                cnt += 1
+        print(f'# Clip duration for {mad_path} probably zero: {cnt}')
+        save(root, mad_transformed_train, mad_transformed_val)
 
 
-def get_video_feats():
-    return h5py.File(f'/nfs/data3/goldhofer/mad_dataset/CLIP_frames_features_5fps.h5', 'r')
+def check_dict(moment_detr_dict):
+    assert len(moment_detr_dict["relevant_windows"][0]) != 0
+    assert len(moment_detr_dict["saliency_scores"]) != 0, "saliency scores are zero"
+    assert moment_detr_dict["relevant_windows"][0][0] < moment_detr_dict["relevant_windows"][0][1]
+    assert 0 <= moment_detr_dict["relevant_windows"][0][0] < moment_detr_dict["relevant_windows"][0][
+        1], f'relevant window: {moment_detr_dict["relevant_windows"][0]}'
 
 
-def _mad_get_video_feat_by_vid(vid, meta, video_feats):
-    video_feat_cache = np.array(video_feats[vid]).astype(np.float32)
+def save(root, mad_transformed_train, mad_transformed_val):
+    with open(f'{root}/annotations/MAD_train_transformed.json', "w") as f:
+        f.write("\n".join([json.dumps(e) for e in mad_transformed_train]))
+    with open(f'{root}/annotations/MAD_val_transformed.json', "w") as f:
+        f.write("\n".join([json.dumps(e) for e in mad_transformed_val]))
 
-    rng = np.random.default_rng(42)
-    video_feat_cache, meta = _slice_window(video_feat_cache, meta, rng)
-    return torch.from_numpy(video_feat_cache), meta  # (Lv, D)
+    print(f'length train: {len(mad_transformed_train)}, length val: {len(mad_transformed_val)}')
 
 
 def _slice_window(frame_features, meta, rng):
@@ -113,11 +109,6 @@ def _check_offsets(f_right_offset, f_left_offset, f_relevant_windows, f_max_v_l,
         f_left_offset += f_right_offset
         f_right_offset = 0
 
-    # assert int(f_relevant_windows[1] + f_right_offset) - int(
-    #    f_relevant_windows[0] - f_left_offset) == f_max_v_l, "Window lengths dont match"
-
-    # assert f_relevant_windows[1] + f_right_offset != f_relevant_windows[0] - f_left_offset, "Zero window length"
-
     return f_right_offset, f_left_offset
 
 
@@ -136,12 +127,11 @@ def _adjust_meta(meta, f_left_offset, f_window_length):
     new_window = [[window_start, int(window_start + f_window_length / 5)]]
     new_clip_ids = [i for i in range(int(new_window[0][0] / 2), int(new_window[0][1] / 2))]
 
-    # assert new_window[1] - new_window[0] == meta["relevant_windows"][1] - meta["relevant_windows"][
-    #    0], "adjusting windows error"
-    # assert len(meta["saliency_scores"]) == len(meta["relevant_clip_ids"]), "adjusting windows saliency error"
-    # assert meta["relevant_windows"][0] / 2 == meta["relevant_clip_ids"][0], "adjusting windows clip id error"
-
     meta["relevant_windows"] = new_window
     meta["relevant_clip_ids"] = new_clip_ids
     # meta.pop("duration")
     return meta
+
+
+if __name__ == "__main__":
+    run()
